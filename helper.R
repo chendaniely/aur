fcd2947092504a34f2544032ba58b39637193185
Daniel Chen <chendaniely@gmail.com>
2020-09-03 16:33:42 -0400

    semi-auto update preview release
    working directory changes in the git add commit push may not work correctly

--- helper.R
+++ helper.R
@@ -29,3 +29,86 @@ get_pkgbuild_name_version <- function(url) {
     )
   )
 }
+
+clone_from_aur <- function(remote_url, save_pth, git_credentials) {
+  #aur_git_pth <- paste(tempdir(), 'rstudio-desktop-preview-bin', sep = '/')
+  #aur_git_pth <- aur_git_pth
+  #remote_url <- "ssh://aur@aur.archlinux.org/rstudio-desktop-daily-bin.git"
+  #remote_url <- "https://aur.archlinux.org/rstudio-desktop-daily-bin.git"
+
+  if (fs::dir_exists(save_pth)) {fs::dir_delete(save_pth)}
+
+  #git_credentials <- git_credentials
+
+  git2r::clone(url = remote_url,
+               local_path = save_pth,
+               credentials = git_credentials)
+
+}
+
+# https://s3.amazonaws.com/rstudio-ide-build/desktop/bionic/amd64/rstudio-1.3.1081-amd64.deb
+
+update_pkgbuild_file <- function(local_clone_pth, deb_url, deb_version) {
+  clone_pkgbuild_pth <- paste(local_clone_pth, "PKGBUILD", sep = "/")
+
+  clone_pkgbuild <- readLines(clone_pkgbuild_pth)
+
+  pkgver_line <- clone_pkgbuild[[11]]
+  sha256sum_line <- clone_pkgbuild[[25]]
+
+  stopifnot(stringr::str_starts(pkgver_line, "pkgver="))
+  stopifnot(stringr::str_starts(sha256sum_line, "sha256sums_x86_64="))
+
+  deb_pth <- paste(local_clone_pth, fs::path_file(deb_url), sep = "/")
+
+  download.file(deb_url, destfile = deb_pth)
+
+  #sha256 <- digest::digest(xenial_deb_pth, algo = "sha256")
+  sha256 <- system2("sha256sum", deb_pth, stdout = TRUE) %>%
+    stringr::str_split_fixed(" ", n = 2) %>%
+    .[,1]
+
+  new_pkgver_line <- stringr::str_replace(pkgver_line,
+                                          pattern = "(?<=\\=).*",
+                                          replacement = deb_version)
+  new_sha256sum_line <- stringr::str_replace(sha256sum_line,
+                                             pattern = "(?<=\\(').*(?='\\))", # between the round brackets
+                                             replacement = sha256)
+
+  clone_pkgbuild[[11]] <- new_pkgver_line
+  clone_pkgbuild[[25]] <- new_sha256sum_line
+
+  writeLines(text = clone_pkgbuild, con = clone_pkgbuild_pth)
+}
+
+mksrcinfo <- function(local_clone_pth) {
+  clone_pkgbuild_pth <- paste(local_clone_pth, "PKGBUILD", sep = "/")
+  .old_wd <- getwd()
+
+  setwd(local_clone_pth)
+  system2("mksrcinfo", clone_pkgbuild_pth)
+  print(fs::dir_ls(all = TRUE))
+  print(git2r::status())
+  print(system2("git",
+                c("diff", "HEAD", "--no-ext-diff", "--unified=0", "--exit-code", "-a", "--no-prefix")))
+  setwd(.old_wd)
+}
+
+git_add_commit_push <- function(local_clone_path,
+                                message,
+                                git_credentials,
+                                push = FALSE) {
+  .old_wd <- getwd()
+
+  setwd(local_clone_path)
+  print(git2r::status())
+
+  git2r::add(repo = '.', path = c("PKGBUILD", ".SRCINFO"))
+  git2r::commit(message = message)
+
+  print(git2r::status())
+
+  if (push) {git2r::push(credentials = git_credentials)}
+
+  setwd(.old_wd)
+}
--- update_rstudio-desktop-preview-bin.R
+++ update_rstudio-desktop-preview-bin.R
@@ -1,5 +1,6 @@
 library(jsonlite)
 library(glue)
+library(git2r)
 
 source("helper.R")
 
@@ -25,4 +26,24 @@ if (rstudio_desktop_preview_bionic$version == pkgbuild_name_version$pkgversion) 
     "AUR version:     {pkgbuild_name_version$pkgversion}
      RStudio version: {rstudio_desktop_preview_bionic$version}"
   ))
+
+  aur_git_pth <- paste(tempdir(), 'rstudio-desktop-preview-bin', sep = '/')
+  remote_url <- "ssh://aur@aur.archlinux.org/rstudio-desktop-preview-bin.git"
+
+  git_credentials <- git2r::cred_ssh_key(
+    publickey = ssh_path("id_rsa.pub"),
+    privatekey = ssh_path("id_rsa"),
+    passphrase = character(0)
+  )
+
+  clone_from_aur(remote_url, aur_git_pth, git_credentials)
+
+  update_pkgbuild_file(aur_git_pth, rstudio_desktop_preview_bionic$s3_url, rstudio_desktop_preview_bionic$version)
+
+  mksrcinfo(aur_git_pth)
+
+  git_add_commit_push(aur_git_pth,
+                      message = glue::glue("Semi-auto update: v{rstudio_desktop_preview_bionic$version}"),
+                      git_credentials,
+                      push = TRUE)
 }
