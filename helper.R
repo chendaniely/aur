9215856c588d5e3904c435a007fd8d406bdfa0fe
Daniel Chen <chendaniely@gmail.com>
2021-09-24 13:32:03 -0400

    aur version shows "+" instead of "%2B"

--- helper.R
+++ helper.R
@@ -20,7 +20,8 @@ get_pkgbuild_name_version <- function(url) {
   pkgver_aur <- pkgversion[[2]] %>%
     stringr::str_split_fixed(pattern = "=", n = 2) %>%
     .[,2] %>%
-    stringr::str_trim()
+    stringr::str_trim() %>%
+    stringr::str_replace_all("%2B", "+")
 
   pkgver_url <- pkgversion[[3]] %>%
     stringr::str_split_fixed(pattern = "=", n = 2) %>%
